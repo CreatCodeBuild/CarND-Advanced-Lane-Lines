6cc511e313c352039d4b4fa5bf8580caf889f59f
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2017-02-05 17:54:48 -0800

    binary threshold seems to be better than adaptive threshold for this task

--- helper.py
+++ helper.py
@@ -42,27 +42,35 @@ def threshold(img, color_space='BGR'):
 	:param color_space: color space of the input image. BGR or RGB
 	:return: thresholded image
 	"""
-	# if color_space == 'BGR':
-	# 	img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-	# elif color_space == 'RGB':
-	# 	img = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-	# elif color_space == 'HLS':
-	# 	pass
-	# else:
-	# 	print('Color Space Error')
-	# 	exit()
+	if color_space == 'BGR':
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+	elif color_space == 'RGB':
+		img = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+	elif color_space == 'HLS':
+		pass
+	else:
+		print('Color Space Error')
+		exit()
 
 	# get S channel
-	# img = img[:, :, 2]
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	img = cv2.blur(img, (7, 7))
-	return cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 71, 3)
+	img = img[:, :, 2]
+	# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	# img = cv2.blur(img, (7, 7))
+
+	cv2.imshow('', img)
+	cv2.waitKey(10000)
+
+	thresh = (240, 255)
+	binary = np.zeros_like(img)
+	binary[(img > thresh[0]) & (img <= thresh[1])] = 255
+
+	return binary
 
 
 # def perspective_transform(img, )
 
 
-def test_calibrator():
+def test_Calibrator():
 	calibrator = Calibrator()
 	folder = 'camera_cal'
 	for path in os.listdir(folder):
@@ -76,5 +84,14 @@ def test_calibrator():
 		cv2.imwrite(os.path.join('output_images/calibration', path), undistorted)
 	print('Calibrator test is done')
 
+
+def test_threshold():
+	img = threshold(cv2.imread('test_images/test6.jpg', cv2.IMREAD_COLOR))
+	cv2.imshow('', img)
+	cv2.waitKey(10000)
+
+def test_perspective_transform():
+	pass
 if __name__ == '__main__':
-	test_calibrator()
+	# test_Calibrator()
+	test_threshold()
